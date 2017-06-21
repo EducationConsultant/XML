2ecbc2ff1c4a520ed5266b7578baf1eab5464ef1
SvetlanaDjuric <cdjuric963@gmail.com>
2017-06-21 20:15:28 +0200

    Ubacen Dozer. Odradjeno mairanje JAXB NalogZaPrenos na java klasu, da bi se moglo snimiti u bazu.

--- XML/firma/src/main/java/com/firma/Application.java
+++ XML/firma/src/main/java/com/firma/Application.java
@@ -3,6 +3,18 @@ package com.firma;
 
 
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,6 +28,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import com.firma.models.nalogzaprenos.NalogZaPrenos;
+import com.firma.models.nalogzaprenos.NalogZaPrenosIzvedeno;
 import com.firma.repository.FirmaRepository;
 
 @EnableJpaRepositories(basePackageClasses=FirmaRepository.class)
@@ -29,7 +43,53 @@ import com.firma.repository.FirmaRepository;
 public class Application {
 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws DatatypeConfigurationException {
+		 List<String> list = new ArrayList<String>();
+		    // Add the mapping configuration
+		    list.add("dozer-bean-mappings.xml");
+		    // Add to DozerMapper
+		    Mapper mapper = new DozerBeanMapper(list);
+
+		    
+		    GregorianCalendar gcal = new GregorianCalendar();
+		    XMLGregorianCalendar xgcal = DatatypeFactory.newInstance().newXMLGregorianCalendar(gcal);
+		    System.err.println("datum" + xgcal);
+		    
+		    Date date = xgcal.toGregorianCalendar().getTime();
+		    
+		    NalogZaPrenos p1Domain = new NalogZaPrenos();
+		    p1Domain.setDatumNaloga(xgcal);
+		    p1Domain.setDatumValute(xgcal);
+		    p1Domain.setDuznikNalogodavac("c");
+		    p1Domain.setHitno(false);
+		    p1Domain.setIDPoruke(new Long(4));
+		    p1Domain.setIznos(new BigDecimal(200));
+		    p1Domain.setModelOdobrenja(22);
+		    p1Domain.setModelZaduzenja(22);
+		    p1Domain.setOznakaValute("DIN");
+		    p1Domain.setPozivNaBrojOdobrenja("asa");
+		    p1Domain.setPozivNaBrojZaduzenja("as");
+		    p1Domain.setPrimalacPoverilac("12");
+		    p1Domain.setRacunDuznika("123456789987654123");
+		    p1Domain.setRacunPoverioca("987456321789654123");
+		    p1Domain.setSvrhaPlacanja("lepo");
+		    
+		    
+		    NalogZaPrenosIzvedeno p1Dto = new NalogZaPrenosIzvedeno();
+
+
+		    System.err.println("before mapping with dozer: p1Dto = " + p1Dto);
+
+		    //map source: p1Domain to target:p1Dto using "dozer-bean-mappings.xml" map-id: person
+		    mapper.map(p1Domain, p1Dto, "person");
+
+		    System.err.println("after mapping with dozer: p1Dto = " + p1Dto);
+		
+		
+		
+		
 		SpringApplication.run(Application.class, args);
 	}
 }
+
+
---
+++ XML/firma/src/main/java/com/firma/models/nalogzaprenos/NalogZaPrenosIzvedeno.java
@@ -0,0 +1,293 @@
+package com.firma.models.nalogzaprenos;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "nalogzaprenos")
+public class NalogZaPrenosIzvedeno {
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idPoruke", nullable = false, unique = true)
+    protected Long idPoruke;
+    
+
+	@Column(name = "duznikNalogodavac", nullable = false)
+	@NotNull
+    protected String duznikNalogodavac;
+ 
+	
+	@Column(name = "svrhaPlacanja", nullable = false)
+	@NotNull
+    protected String svrhaPlacanja;
+
+    
+	@Column(name = "primalacPoverilac", nullable = false)
+	@NotNull
+    protected String primalacPoverilac;
+
+    
+	@Temporal(TemporalType.DATE)
+	@Column(name = "datumNaloga", nullable = false)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	@NotNull
+    protected Date datumNaloga;
+ 
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "datumValute", nullable = false)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	@NotNull
+    protected Date datumValute;
+   
+    
+	@Column(name = "racunDuznika", nullable = false)
+	@NotNull
+    protected String racunDuznika;
+   
+    
+	@Column(name = "modelZaduzenja", nullable = false)
+	@NotNull
+    protected int modelZaduzenja;
+
+    
+	@Column(name = "pozivNaBrojZaduzenja", nullable = false)
+	@NotNull
+    protected String pozivNaBrojZaduzenja;
+ 
+    
+	@Column(name = "racunPoverioca", nullable = false)
+	@NotNull
+    protected String racunPoverioca;
+    
+	
+	@Column(name = "modelOdobrenja", nullable = false)
+	@NotNull
+    protected int modelOdobrenja;
+
+	
+	@Column(name = "pozivNaBrojOdobrenja", nullable = false)
+	@NotNull
+    protected String pozivNaBrojOdobrenja;
+
+	@Column(name = "iznos", nullable = false)
+	@NotNull
+    protected BigDecimal iznos;
+
+	
+	@Column(name = "oznakaValute", nullable = false)
+	@NotNull
+    protected String oznakaValute;
+
+	
+	
+	@Column(name = "hitno", nullable = false)
+	@NotNull
+    protected boolean hitno;
+
+	 
+	 
+	 public NalogZaPrenosIzvedeno() {
+		 
+	 }
+
+
+
+	public Long getIdPoruke() {
+		return idPoruke;
+	}
+
+
+
+	public void setIdPoruke(Long idPoruke) {
+		this.idPoruke = idPoruke;
+	}
+
+
+
+	public String getDuznikNalogodavac() {
+		return duznikNalogodavac;
+	}
+
+
+
+	public void setDuznikNalogodavac(String duznikNalogodavac) {
+		this.duznikNalogodavac = duznikNalogodavac;
+	}
+
+
+
+	public String getSvrhaPlacanja() {
+		return svrhaPlacanja;
+	}
+
+
+
+	public void setSvrhaPlacanja(String svrhaPlacanja) {
+		this.svrhaPlacanja = svrhaPlacanja;
+	}
+
+
+
+	public String getPrimalacPoverilac() {
+		return primalacPoverilac;
+	}
+
+
+
+	public void setPrimalacPoverilac(String primalacPoverilac) {
+		this.primalacPoverilac = primalacPoverilac;
+	}
+
+
+
+	public Date getDatumNaloga() {
+		return datumNaloga;
+	}
+
+
+
+	public void setDatumNaloga(Date datumNaloga) {
+		this.datumNaloga = datumNaloga;
+	}
+
+
+
+	public Date getDatumValute() {
+		return datumValute;
+	}
+
+
+
+	public void setDatumValute(Date datumValute) {
+		this.datumValute = datumValute;
+	}
+
+
+
+	public String getRacunDuznika() {
+		return racunDuznika;
+	}
+
+
+
+	public void setRacunDuznika(String racunDuznika) {
+		this.racunDuznika = racunDuznika;
+	}
+
+
+
+	public int getModelZaduzenja() {
+		return modelZaduzenja;
+	}
+
+
+
+	public void setModelZaduzenja(int modelZaduzenja) {
+		this.modelZaduzenja = modelZaduzenja;
+	}
+
+
+
+	public String getPozivNaBrojZaduzenja() {
+		return pozivNaBrojZaduzenja;
+	}
+
+
+
+	public void setPozivNaBrojZaduzenja(String pozivNaBrojZaduzenja) {
+		this.pozivNaBrojZaduzenja = pozivNaBrojZaduzenja;
+	}
+
+
+
+	public String getRacunPoverioca() {
+		return racunPoverioca;
+	}
+
+
+
+	public void setRacunPoverioca(String racunPoverioca) {
+		this.racunPoverioca = racunPoverioca;
+	}
+
+
+
+	public int getModelOdobrenja() {
+		return modelOdobrenja;
+	}
+
+
+
+	public void setModelOdobrenja(int modelOdobrenja) {
+		this.modelOdobrenja = modelOdobrenja;
+	}
+
+
+
+	public String getPozivNaBrojOdobrenja() {
+		return pozivNaBrojOdobrenja;
+	}
+
+
+
+	public void setPozivNaBrojOdobrenja(String pozivNaBrojOdobrenja) {
+		this.pozivNaBrojOdobrenja = pozivNaBrojOdobrenja;
+	}
+
+
+
+	public BigDecimal getIznos() {
+		return iznos;
+	}
+
+
+
+	public void setIznos(BigDecimal iznos) {
+		this.iznos = iznos;
+	}
+
+
+
+	public String getOznakaValute() {
+		return oznakaValute;
+	}
+
+
+
+	public void setOznakaValute(String oznakaValute) {
+		this.oznakaValute = oznakaValute;
+	}
+
+
+
+	public boolean isHitno() {
+		return hitno;
+	}
+
+
+
+	public void setHitno(boolean hitno) {
+		this.hitno = hitno;
+	}
+
+	 
+	   
+}
