c854befdc127a2c374aeeb0abf4a741ee7079394
MILENA-PC\Milena <mlalic994@gmail.com>
2017-06-22 15:36:05 +0200

    Konekcija sa bazom za banku

--- XML/banka/src/main/java/com/banka/BankaApplication.java
+++ XML/banka/src/main/java/com/banka/BankaApplication.java
@@ -1,12 +1,16 @@
-package com.banka.banka;
+package com.banka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.banka.repository.BankaRepository;
 
 @SpringBootApplication
 @ComponentScan("com.banka.controllers")
+@EnableJpaRepositories(basePackageClasses = BankaRepository.class)
 @EnableAutoConfiguration
 public class BankaApplication {
 
