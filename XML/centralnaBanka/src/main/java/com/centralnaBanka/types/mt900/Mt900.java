c9d8c967a9ffb9b6eeedf418b98be9fc9cb7550b
Stefan Boskovic <stefanbole94@gmail.com>
2017-06-23 22:35:24 +0200

    Centralna Banka

---
+++ XML/centralnaBanka/src/main/java/com/centralnaBanka/types/mt900/Mt900.java
@@ -0,0 +1,276 @@
+
+package com.centralnaBanka.types.mt900;
+
+import java.math.BigDecimal;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="ID_poruke"&gt;
+ *           &lt;simpleType&gt;
+ *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string"&gt;
+ *               &lt;maxLength value="50"/&gt;
+ *             &lt;/restriction&gt;
+ *           &lt;/simpleType&gt;
+ *         &lt;/element&gt;
+ *         &lt;element name="SWIFT_kod_banke_duznika"&gt;
+ *           &lt;simpleType&gt;
+ *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string"&gt;
+ *               &lt;length value="8"/&gt;
+ *             &lt;/restriction&gt;
+ *           &lt;/simpleType&gt;
+ *         &lt;/element&gt;
+ *         &lt;element name="Obracunski_racun_banke_duznika"&gt;
+ *           &lt;simpleType&gt;
+ *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string"&gt;
+ *               &lt;length value="20"/&gt;
+ *             &lt;/restriction&gt;
+ *           &lt;/simpleType&gt;
+ *         &lt;/element&gt;
+ *         &lt;element name="ID_poruke_naloga"&gt;
+ *           &lt;simpleType&gt;
+ *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string"&gt;
+ *               &lt;length value="50"/&gt;
+ *             &lt;/restriction&gt;
+ *           &lt;/simpleType&gt;
+ *         &lt;/element&gt;
+ *         &lt;element name="Datum_valute"&gt;
+ *           &lt;simpleType&gt;
+ *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}date"&gt;
+ *             &lt;/restriction&gt;
+ *           &lt;/simpleType&gt;
+ *         &lt;/element&gt;
+ *         &lt;element name="Iznos"&gt;
+ *           &lt;simpleType&gt;
+ *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}decimal"&gt;
+ *               &lt;totalDigits value="15"/&gt;
+ *               &lt;fractionDigits value="2"/&gt;
+ *             &lt;/restriction&gt;
+ *           &lt;/simpleType&gt;
+ *         &lt;/element&gt;
+ *         &lt;element name="Sifra_valute"&gt;
+ *           &lt;simpleType&gt;
+ *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string"&gt;
+ *               &lt;length value="3"/&gt;
+ *             &lt;/restriction&gt;
+ *           &lt;/simpleType&gt;
+ *         &lt;/element&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "idPoruke",
+    "swiftKodBankeDuznika",
+    "obracunskiRacunBankeDuznika",
+    "idPorukeNaloga",
+    "datumValute",
+    "iznos",
+    "sifraValute"
+})
+@XmlRootElement(name = "mt900")
+public class Mt900 {
+
+    @XmlElement(name = "ID_poruke", required = true)
+    protected String idPoruke;
+    @XmlElement(name = "SWIFT_kod_banke_duznika", required = true)
+    protected String swiftKodBankeDuznika;
+    @XmlElement(name = "Obracunski_racun_banke_duznika", required = true)
+    protected String obracunskiRacunBankeDuznika;
+    @XmlElement(name = "ID_poruke_naloga", required = true)
+    protected String idPorukeNaloga;
+    @XmlElement(name = "Datum_valute", required = true)
+    protected XMLGregorianCalendar datumValute;
+    @XmlElement(name = "Iznos", required = true)
+    protected BigDecimal iznos;
+    @XmlElement(name = "Sifra_valute", required = true)
+    protected String sifraValute;
+
+    /**
+     * Gets the value of the idPoruke property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getIDPoruke() {
+        return idPoruke;
+    }
+
+    /**
+     * Sets the value of the idPoruke property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setIDPoruke(String value) {
+        this.idPoruke = value;
+    }
+
+    /**
+     * Gets the value of the swiftKodBankeDuznika property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSWIFTKodBankeDuznika() {
+        return swiftKodBankeDuznika;
+    }
+
+    /**
+     * Sets the value of the swiftKodBankeDuznika property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSWIFTKodBankeDuznika(String value) {
+        this.swiftKodBankeDuznika = value;
+    }
+
+    /**
+     * Gets the value of the obracunskiRacunBankeDuznika property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getObracunskiRacunBankeDuznika() {
+        return obracunskiRacunBankeDuznika;
+    }
+
+    /**
+     * Sets the value of the obracunskiRacunBankeDuznika property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setObracunskiRacunBankeDuznika(String value) {
+        this.obracunskiRacunBankeDuznika = value;
+    }
+
+    /**
+     * Gets the value of the idPorukeNaloga property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getIDPorukeNaloga() {
+        return idPorukeNaloga;
+    }
+
+    /**
+     * Sets the value of the idPorukeNaloga property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setIDPorukeNaloga(String value) {
+        this.idPorukeNaloga = value;
+    }
+
+    /**
+     * Gets the value of the datumValute property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public XMLGregorianCalendar getDatumValute() {
+        return datumValute;
+    }
+
+    /**
+     * Sets the value of the datumValute property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public void setDatumValute(XMLGregorianCalendar value) {
+        this.datumValute = value;
+    }
+
+    /**
+     * Gets the value of the iznos property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public BigDecimal getIznos() {
+        return iznos;
+    }
+
+    /**
+     * Sets the value of the iznos property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public void setIznos(BigDecimal value) {
+        this.iznos = value;
+    }
+
+    /**
+     * Gets the value of the sifraValute property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSifraValute() {
+        return sifraValute;
+    }
+
+    /**
+     * Sets the value of the sifraValute property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSifraValute(String value) {
+        this.sifraValute = value;
+    }
+
+}
