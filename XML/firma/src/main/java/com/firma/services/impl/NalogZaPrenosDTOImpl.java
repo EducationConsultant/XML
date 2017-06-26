722de551d105192f1f45aad551bf47be30fb01e7
SvetlanaDjuric <cdjuric963@gmail.com>
2017-06-26 13:46:55 +0200

    Prebacen kod iz kontrolera firme u servis.

--- XML/firma/src/main/java/com/firma/services/impl/NalogZaPrenosDTOImpl.java
+++ XML/firma/src/main/java/com/firma/services/impl/NalogZaPrenosDTOImpl.java
@@ -1,13 +1,18 @@
 package com.firma.services.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.firma.models.domain.FakturaDTO;
 import com.firma.models.domain.NalogZaPrenosDTO;
+import com.firma.models.nalogzaprenos.NalogZaPrenos;
 import com.firma.repository.NalogZaPrenosDTORepository;
 import com.firma.services.NalogZaPrenosDTOService;
 
@@ -31,6 +36,35 @@ public class NalogZaPrenosDTOImpl implements NalogZaPrenosDTOService {
 	@Override
 	public List<NalogZaPrenosDTO> find() {
 		return repository.findAll();
+	}
+
+	@Override
+	public NalogZaPrenos kreirajNalog(FakturaDTO faktura) {
+		
+		NalogZaPrenosDTO nalogzaprenos = new NalogZaPrenosDTO();
+		nalogzaprenos.setDatumNaloga(faktura.getDatumRacuna());
+		nalogzaprenos.setDatumValute(faktura.getDatumValute());
+		nalogzaprenos.setIznos(faktura.getIznosZaUplatu());
+		nalogzaprenos.setOznakaValute(faktura.getOznakaValute());
+		nalogzaprenos.setRacunDuznika("123654789987456321");
+		nalogzaprenos.setRacunPoverioca(faktura.getUplataNaRacun());
+		nalogzaprenos.setDuznikNalogodavac(faktura.getNazivKupca());
+		nalogzaprenos.setPrimalacPoverilac(faktura.getNazivDobavljaca());
+		nalogzaprenos.setSvrhaPlacanja("racun za februar");
+		nalogzaprenos.setModelOdobrenja(97);
+		nalogzaprenos.setModelZaduzenja(97);
+		nalogzaprenos.setPozivNaBrojOdobrenja("12456");
+		nalogzaprenos.setPozivNaBrojZaduzenja("7895");
+		nalogzaprenos.setHitno(false);
+		NalogZaPrenosDTO nalog = this.save(nalogzaprenos);
+
+		NalogZaPrenos n = new NalogZaPrenos();
+		List<String> list = new ArrayList<String>();
+		list.add("dozer-bean-mappings.xml");
+		Mapper mapper = new DozerBeanMapper(list);
+		mapper.map(nalog, n, "person");
+		
+		return n;
 	}
 
 }
