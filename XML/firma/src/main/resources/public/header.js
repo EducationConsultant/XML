a4746fbd929e9127e9e399b93d931f6b49eab865
Stefan Boskovic <stefanbole94@gmail.com>
2017-06-27 09:48:00 +0200

    header

---
+++ XML/firma/src/main/resources/public/header.js
@@ -0,0 +1,11 @@
+'use strict';
+
+app.component('ourHeader', {
+    templateUrl: 'header.html',
+    controller: [ '$scope', function HeaderController( $scope) {
+        
+        
+
+
+    }]
+});
